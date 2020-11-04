b81e5db3f707a2befca37e6f725fcee5ceca13db
Khue Doan <khuedoan98@gmail.com>
2020-11-04 23:19:17 +0700

    Move mac addresses to environment variable

--- wakeonlan/docker-entrypoint.sh
+++ wakeonlan/docker-entrypoint.sh
@@ -4,7 +4,4 @@ until curl -s 127.0.0.1 > /dev/null; do
   echo "HTTP server is not ready"
 done
 
-awake 00:23:24:d1:f3:f0 \
-      00:23:24:d1:f4:d6 \
-      00:23:24:d1:f5:69 \
-      00:23:24:e7:04:60
+awake $MAC_ADDRESSES
